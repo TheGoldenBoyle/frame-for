038547d049c350c6ec7a9cbfbab3045e7f930082
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2025-11-02 20:31:39 +0100

    Fix

--- app/dashboard/playground/layout.tsx
+++ app/dashboard/playground/layout.tsx
@@ -11,7 +11,7 @@ export default function PlaygroundLayout({
                 <div className="particle particle-3"></div>
             </div>
             <div className="min-h-[calc(100vh-72px)] relative z-10">
-                <div className="h-full flex flex-col mx-auto">
+                <div className="h-full flex flex-col mx-auto px-2">
                     {children}
                 </div>
             </div>
